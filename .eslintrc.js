412cdbf833ec1e6d4f1548dd3a0b68caea62e881
Lluc Valério <lluc.valerio@gmail.com>
2023-12-19 19:39:21 +0100

    Add new rule to lintern

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,6 +26,7 @@ module.exports = {
     'comma-dangle': [2, 'never'],
     'linebreak-style': 0,
     'global-require': 0,
-    'eslint linebreak-style': [0, 'error', 'windows']
+    'eslint linebreak-style': [0, 'error', 'windows'],
+    'no-restricted-syntax': ['error', 'FunctionExpression', 'WithStatement', "BinaryExpression[operator='in']"]
   }
 };
